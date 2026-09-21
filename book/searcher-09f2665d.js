7d70cd950dc37990a99a3e4c75c71451809aab5c
C. Sean Burns <sean.csb@gmail.com>
2026-09-21 02:52:06 -0400

    Rebuild mdbook

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e2f58ff0.js',
+                path_to_root + 'searchindex-0cd641c1.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
